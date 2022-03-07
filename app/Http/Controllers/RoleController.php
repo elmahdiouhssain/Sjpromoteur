2cf999e98f7f4c27250ad6464d4dea44ed89e3f1
elmahdiouhssain <elmahdi.ouhssain@gmail.com>
2022-03-07 17:57:49 +0100

    fixing errors related

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -13,6 +13,13 @@ use DataTables;
     
 class RoleController extends Controller
 {
+    function __construct()
+    {
+        $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+        $this->middleware('permission:role-create', ['only' => ['create','store']]);
+        $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+        $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
 
     public function Rolelistajax(Request $request){
     if ($request->ajax()) {
